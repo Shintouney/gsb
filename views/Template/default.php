6da0af6c233caeedf8c0e709fbcdffe55a91b687
Avinint <avinint@hotmail.com>
2017-01-17 14:23:33 +0100

    fix

--- views/Template/default.php
+++ views/Template/default.php
@@ -11,6 +11,6 @@
     <?php include 'footer.php' ?>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="<?php echo $this->asset('js/bootstrap.min.js')?>"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
