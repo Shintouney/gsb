08d3fb4c305ab448fc201e5a447b42481f8e6907
Avinint <avinint@hotmail.com>
2017-01-28 14:31:23 +0100

    fixes

--- views/User/import.php
+++ views/User/import.php
@@ -1,4 +1,10 @@
-<form action="" method="post" enctype="multipart/form-data">
-    <div><input type="file" name="file" /></div>
-    <input type="submit" value="Importer"/>
-</form>
+<div class="box">
+    <fieldset class="align-fieldset fieldset-auto-width">
+        <legend> <span class='icon fa-pencil-square-o'>fichier csv</span></legend>
+        <form action="" method="post" enctype="multipart/form-data">
+        <div><input type="file" name="file" /></div>
+            <hr/>
+        <input type="submit" value="Importer"/>
+        </form>
+    </fieldset>
+</div>
