2226f1bb2fe328f037586e49c7cf6dd16c490700
kishiro <uchiwa43@hotmail.fr>
2017-05-12 18:28:34 +0200

    rush 3

--- views/Materiel/show.php
+++ views/Materiel/show.php
@@ -1,27 +1,13 @@
 <table>
-	<tr><td>Id</td>							<td><?php echo $ticket['id'];?> 					</td></tr>
-	<tr><td>Etat</td>						<td><?php echo $ticket['intitule_etat'];?> 			</td></tr>
-	<tr><td>Type du materiel</td>			<td><?php echo $ticket['type_materiel'];?> 			</td></tr>
- 	<tr><td>Marque du materiel</td>			<td><?php echo $ticket['marque_materiel'];?> 		</td></tr>
-  	<tr><td>Modèle du materiel</td>			<td><?php echo $ticket['modele_materiel'];?> 		</td></tr>
-	<tr><td>Objet de l'incident</td>		<td><?php echo $ticket['objet_incident'];?> 		</td></tr>
-	<tr><td>Description de l'incident</td>	<td><?php echo $ticket['description_incident'];?> 	</td></tr>
-	<tr><td>Solution de lm'incident</td>		<td><?php echo $ticket['solution_incident'];?> 		</td></tr>
-	<tr><td>Date de signalement </td>		<td><?php echo $ticket['date_signalement'];?> 		</td></tr>
-	<tr><td>Date d'intervention</td>		<td><?php echo $ticket['date_intervention'];?> 		</td></tr>
-	<tr><td>Salle</td>						<td><?php echo $ticket['salle_nom'] ;?>				</td></tr>
+	<tr><td>id_materiel</td>			<td><?php echo $un_materiel['id_materiel'];?> 		</td></tr>
+	<tr><td>type</td>					<td><?php echo $un_materiel['type'];?> 			</td></tr>
+	<tr><td>marque</td>					<td><?php echo $un_materiel['marque'];?> 			</td></tr>
+ 	<tr><td>modele</td>					<td><?php echo $un_materiel['modele'];?> 		</td></tr>
+  	<tr><td>logiciels_installes</td>	<td><?php echo $un_materiel['logiciels_installes'];?> 	</td></tr>
+	<tr><td>date_achat</td>				<td><?php echo $un_materiel['date_achat'];?> 		</td></tr>
+	<tr><td>date_fin_garantie</td>		<td><?php echo $un_materiel['date_fin_garantie'];?> 	</td></tr>
+	<tr><td>num_inventaire</td>			<td><?php echo $un_materiel['num_inventaire'];?> 		</td></tr>
 	
-	<?php
-	if ($this->getUser()->is(array('ROLE_TECHNICIEN', 'ROLE_RESPONSABLE', 'ROLE_ADMIN')))
-	{
-	?>
-		<tr><td>Technicien </td><td><?php echo $ticket['pnom_tech'] . ' ' . $ticket['nom_tech'];?></td></tr>
-		<tr><td>Demandeur</td>				<td><?php echo $ticket['pnom_demand'] . ' ' . $ticket['nom_demand'];?></td></tr>
-		<tr><td>Niveau d'urgence</td>		<td><?php echo $ticket['niveau_urgence'];?>			</td></tr>
-		<tr><td>Niveau de complexité</td>	<td><?php echo $ticket['niveau_complexite'];?>		</td></tr>
-		<tr><td>Durée</td>					<td><?php echo $ticket['duree'];?>					</td></tr>
-		<tr><td>Nombre d'appels</td>		<td><?php echo $ticket['nb_appels'];?>				</td></tr>
-
 		<p><!--modifier et supprimer-->
 			<a href='?page=incident&action=modifier_ticket&id=<?php echo $ticket['id'];?>'>Modifier le ticket</a>
 
@@ -31,7 +17,5 @@
 				<input type='submit' value='Supprimer le ticket'>
 			</form>
 		</p>
-	<?php
-	}
-	?>
+	
 </table>
